51b7293049eb0678d6f0cdb89fd4c58b48da80a7
guifroes1984 <guilhermefroes26@gmail.com>
2023-04-29 19:40:04 -0300

    Usando JPA puro

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.guifroes.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,15 +15,15 @@ import br.com.guifroes.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 11");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51e3KdrHuCL._AC_SL1080_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Xiaomi-Redmi-Note-11-Graphite/dp/B09QSB4N2C/ref=sr_1_1?keywords=xiaomi+redmi+note+8+pro+128gb&qid=1682795954&sprefix=xiaomi+redmi+note+8%2Caps%2C215&sr=8-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-		pedido.setDescricao("Uma descrição qualquer para esse pedido");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+		
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
--- mudi/src/main/java/br/com/guifroes/mvc/mudi/model/Pedido.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/model/Pedido.java
@@ -3,8 +3,18 @@ package br.com.guifroes.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class Pedido {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataEntrega;
@@ -58,6 +68,14 @@ public class Pedido {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }
