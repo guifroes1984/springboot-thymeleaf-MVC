97ed119c44bb32cfb578ae63e981f7c5b63852c7
guifroes1984 <guilhermefroes26@gmail.com>
2023-04-29 16:39:26 -0300

    Percorrendo a lista na view.

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,28 @@
 package br.com.guifroes.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.guifroes.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi Note 11");
+		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51e3KdrHuCL._AC_SL1080_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Xiaomi-Redmi-Note-11-Graphite/dp/B09QSB4N2C/ref=sr_1_1?keywords=xiaomi+redmi+note+8+pro+128gb&qid=1682795954&sprefix=xiaomi+redmi+note+8%2Caps%2C215&sr=8-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
+		pedido.setDescricao("Uma descrição qualquer para esse pedido");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos", pedidos);
+		
 		return "home";
 	}
 
