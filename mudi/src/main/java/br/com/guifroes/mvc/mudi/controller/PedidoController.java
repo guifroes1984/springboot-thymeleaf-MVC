d6b31ed0e1f638547cf6d84d38d558e78316314d
guifroes1984 <guilhermefroes26@gmail.com>
2023-04-30 19:06:12 -0300

    Salvando pedido.

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/PedidoController.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,33 @@
 package br.com.guifroes.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.guifroes.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.guifroes.mvc.mudi.model.Pedido;
+import br.com.guifroes.mvc.mudi.repository.PedidoRepositoy;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 	
+	@Autowired
+	private PedidoRepositoy pedidoRepositoy;
+	
 	@GetMapping("formulario")
 	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(RequisicaoNovoPedido requisicao) {
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepositoy.save(pedido);
+		
 		return "pedido/formulario";
 	}
 
