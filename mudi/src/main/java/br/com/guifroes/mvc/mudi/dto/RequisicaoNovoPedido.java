a7937c3e2ce5802c50e67227c29f464645f437d2
guifroes1984 <guilhermefroes26@gmail.com>
2023-04-30 19:27:55 -0300

    Validação do pedido.

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -1,11 +1,18 @@
 package br.com.guifroes.mvc.mudi.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import br.com.guifroes.mvc.mudi.model.Pedido;
 
 public class RequisicaoNovoPedido {
-
+	
+	@NotBlank
 	private String nomeProduto;
+	
+	@NotBlank
 	private String urlProduto;
+	
+	@NotBlank
 	private String urlImagem;
 	private String descricao;
 
