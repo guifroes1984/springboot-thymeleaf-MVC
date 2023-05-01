e18e247521a92ebe8dde51a9fadb87225cb6ab4d
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-01 17:41:45 -0300

    Associando o usuário ao pedido.

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/model/Pedido.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/model/Pedido.java
@@ -95,5 +95,13 @@ public class Pedido {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
