e9c8740ae5aae58528cd742514dd4d886a16db9e
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-01 10:48:05 -0300

    Rotas do status.

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/repository/PedidoRepositoy.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/repository/PedidoRepositoy.java
@@ -1,11 +1,16 @@
 package br.com.guifroes.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.guifroes.mvc.mudi.model.Pedido;
+import br.com.guifroes.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepositoy extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 	
 	}
