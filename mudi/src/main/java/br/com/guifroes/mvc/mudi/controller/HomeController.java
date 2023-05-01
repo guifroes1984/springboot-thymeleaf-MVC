b940221f691e1289262e4d34c22e7a02e5a14cf3
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-01 16:48:56 -0300

    Mostrando os pedidos do usuário.

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/controller/HomeController.java
@@ -1,5 +1,6 @@
 package br.com.guifroes.mvc.mudi.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,8 @@ public class HomeController {
 	private PedidoRepositoy pedidoRepositoy;
 
 	@GetMapping
-	public String home(Model model) {
-		List<Pedido> pedidos = pedidoRepositoy.findAll();
+	public String home(Model model, Principal principal) {
+		List<Pedido> pedidos = pedidoRepositoy.findAllByUsuario(principal.getName());
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
