698aa91fffa66989544e900c3a9fe7aade347855
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-01 23:00:41 -0300

    Ajustando a navegação, e resolvendo alguns bugs.

--- mudi/src/main/java/br/com/guifroes/mvc/mudi/WebSecurityConfig.java
+++ mudi/src/main/java/br/com/guifroes/mvc/mudi/WebSecurityConfig.java
@@ -21,15 +21,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-			.anyRequest().authenticated()
+		.antMatchers("/home/**")
+			.permitAll()
+		.anyRequest()
+			.authenticated()
 		.and()
 		.formLogin(form -> form
             .loginPage("/login")
             .defaultSuccessUrl("/usuario/pedido", true)
             .permitAll()
 		)
-		.logout(logout -> logout.logoutUrl("/logout"))
-		.csrf().disable();
+		.logout(logout -> {
+			logout.logoutUrl("/logout")
+				.logoutSuccessUrl("/home");
+		});
+		
 	}
 	
 	@Override
